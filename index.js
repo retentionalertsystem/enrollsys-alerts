ae98ede7869f18a1ac061eb6622d60af18b78952
retentionalertsystem <retentionalertsystem@gmail.com>
2026-03-16 10:49:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import fetch from "node-fetch";
 import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
-import emailjs from "@emailjs/browser";
+import emailjs from "@emailjs/nodejs";
 
 // Add this helper function for delay
 function sleep(ms) {
