2a738684d5ff4a9ca732e81f2daf02aad5a20f52
retentionalertsystem <retentionalertsystem@gmail.com>
2026-03-16 11:27:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ async function sendAlertEmail(alert) {
         "Authorization": `Bearer ${process.env.EMAILJS_PRIVATE_KEY}` // use your private key
       },
       body: JSON.stringify({
-        EMAILJS_SERVICE_ID,
-        EMAILJS_TEMPLATE_ID,
-        EMAILJS_PUBLIC_KEY,
+        process.env.EMAILJS_SERVICE_ID,
+        process.env.EMAILJS_TEMPLATE_ID,
+        process.env.EMAILJS_PUBLIC_KEY,
         templateParams
       })
     });
